a6f441fa473abe8155b5ab0b30c7847cf4ef2371
Luiz Fernando <130947126+OLuizFernando@users.noreply.github.com>
2024-10-08 18:24:55 +0000

    changing the `index.js` (home) file

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,17 @@
 function Home() {
   return (
     <>
-      <h1>Frase Impactante</h1>
-      <p>Conteúdo da página alterado por questões de privacidade...</p>
+      <h1>🚧BookNest está chegando em breve!🚧</h1>
+      <p>
+        Prepare-se para descobrir um novo espaço acolhedor dedicado a leitores
+        apaixonados! 📖✨
+      </p>
+      <p>
+        No <strong>BookNest</strong>, você vai encontrar uma comunidade para
+        trocar ideias sobre livros, descobrir novas histórias e compartilhar
+        suas leituras favoritas de um jeito leve e divertido.
+      </p>
+      <p>Nos vemos em breve! 📚🪶</p>
     </>
   );
 }
