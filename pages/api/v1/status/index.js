1b71830b52a65e43982c561fd6fafc3c816e0bb5
Luiz Fernando <130947126+OLuizFernando@users.noreply.github.com>
2024-10-16 13:21:23 +0000

    adding data to `/status` endpoint and testing it

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,9 +1,24 @@
 import database from "/infra/database.js";
 
 async function status(request, response) {
-  const result = await database.query("SELECT 1 + 1 as sum;");
-  console.log(result.rows);
-  response.status(200).json({ chave: "valor" });
+  const updatedAt = new Date().toISOString();
+
+  const databaseVersion = await database.query("SELECT version();");
+
+  const openedDatabaseConnections = await database.query(
+    "SELECT COUNT(*) AS opened_connections FROM pg_stat_activity WHERE state = 'active';",
+  );
+
+  const maxDatabaseConnections = await database.query("SHOW max_connections;");
+
+  response.status(200).json({
+    updated_at: updatedAt,
+    database: {
+      max_connections: maxDatabaseConnections.rows[0].max_connections,
+      opened_connections: openedDatabaseConnections.rows[0].opened_connections,
+      version: databaseVersion.rows[0].version,
+    },
+  });
 }
 
 export default status;
--- tests/integration/api/v1/status/get.test.js
+++ tests/integration/api/v1/status/get.test.js
@@ -1,4 +1,20 @@
 test("GET to /api/v1/status should return 200", async () => {
   const response = await fetch("http://localhost:3000/api/v1/status");
   expect(response.status).toBe(200);
+
+  const responseBody = await response.json();
+  expect(responseBody.updated_at).toBeDefined();
+  expect(responseBody.database.max_connections).toBeDefined();
+  expect(responseBody.database.opened_connections).toBeDefined();
+  expect(responseBody.database.version).toBeDefined();
+
+  const parsedUpdatedAt = new Date(responseBody.updated_at).toISOString();
+  expect(responseBody.updated_at).toEqual(parsedUpdatedAt);
+
+  expect(responseBody.database.max_connections).toBe("100");
+  expect(responseBody.database.opened_connections).toBe("1");
+
+  const databaseVersion =
+    "PostgreSQL 16.0 on x86_64-pc-linux-musl, compiled by gcc (Alpine 12.2.1_git20220924-r10) 12.2.1 20220924, 64-bit";
+  expect(responseBody.database.version).toBe(databaseVersion);
 });
