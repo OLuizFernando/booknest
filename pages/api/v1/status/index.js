4c0afe05316cf72ec4ca07cad7d09705373f6b29
Luiz Fernando <130947126+OLuizFernando@users.noreply.github.com>
2024-10-17 02:19:46 +0000

    parametrizing database name in `openedDatabaseConnections` query

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -5,9 +5,11 @@ async function status(request, response) {
 
   const databaseVersion = await database.query("SHOW server_version;");
 
-  const openedDatabaseConnections = await database.query(
-    "SELECT COUNT(*) AS opened_connections FROM pg_stat_activity WHERE datname = 'local_db';",
-  );
+  const databaseName = process.env.POSTGRES_DB;
+  const openedDatabaseConnections = await database.query({
+    text: "SELECT COUNT(*) AS opened_connections FROM pg_stat_activity WHERE datname = $1;",
+    values: [databaseName],
+  });
 
   const maxDatabaseConnections = await database.query("SHOW max_connections;");
 
