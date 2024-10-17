1933c0303e2424060408ff99ff852cd69bc784aa
Luiz Fernando <130947126+OLuizFernando@users.noreply.github.com>
2024-10-17 01:50:59 +0000

    improving `/status` endpoint data

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -3,20 +3,26 @@ import database from "/infra/database.js";
 async function status(request, response) {
   const updatedAt = new Date().toISOString();
 
-  const databaseVersion = await database.query("SELECT version();");
+  const databaseVersion = await database.query("SHOW server_version;");
 
   const openedDatabaseConnections = await database.query(
-    "SELECT COUNT(*) AS opened_connections FROM pg_stat_activity WHERE state = 'active';",
+    "SELECT COUNT(*) AS opened_connections FROM pg_stat_activity WHERE datname = 'local_db';",
   );
 
   const maxDatabaseConnections = await database.query("SHOW max_connections;");
 
   response.status(200).json({
     updated_at: updatedAt,
-    database: {
-      max_connections: maxDatabaseConnections.rows[0].max_connections,
-      opened_connections: openedDatabaseConnections.rows[0].opened_connections,
-      version: databaseVersion.rows[0].version,
+    dependencies: {
+      database: {
+        max_connections: parseInt(
+          maxDatabaseConnections.rows[0].max_connections,
+        ),
+        opened_connections: parseInt(
+          openedDatabaseConnections.rows[0].opened_connections,
+        ),
+        version: databaseVersion.rows[0].server_version,
+      },
     },
   });
 }
--- tests/integration/api/v1/status/get.test.js
+++ tests/integration/api/v1/status/get.test.js
@@ -3,18 +3,20 @@ test("GET to /api/v1/status should return 200", async () => {
   expect(response.status).toBe(200);
 
   const responseBody = await response.json();
+
+  expect(Object.keys(responseBody).length).toBe(2);
+  expect(Object.keys(responseBody.dependencies).length).toBe(1);
+  expect(Object.keys(responseBody.dependencies.database).length).toBe(3);
+
   expect(responseBody.updated_at).toBeDefined();
-  expect(responseBody.database.max_connections).toBeDefined();
-  expect(responseBody.database.opened_connections).toBeDefined();
-  expect(responseBody.database.version).toBeDefined();
+  expect(responseBody.dependencies.database.max_connections).toBeDefined();
+  expect(responseBody.dependencies.database.opened_connections).toBeDefined();
+  expect(responseBody.dependencies.database.version).toBeDefined();
 
   const parsedUpdatedAt = new Date(responseBody.updated_at).toISOString();
   expect(responseBody.updated_at).toEqual(parsedUpdatedAt);
 
-  expect(responseBody.database.max_connections).toBe("100");
-  expect(responseBody.database.opened_connections).toBe("1");
-
-  const databaseVersion =
-    "PostgreSQL 16.0 on x86_64-pc-linux-musl, compiled by gcc (Alpine 12.2.1_git20220924-r10) 12.2.1 20220924, 64-bit";
-  expect(responseBody.database.version).toBe(databaseVersion);
+  expect(responseBody.dependencies.database.max_connections).toBe(100);
+  expect(responseBody.dependencies.database.opened_connections).toBe(1);
+  expect(responseBody.dependencies.database.version).toBe("16.0");
 });
