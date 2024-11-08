8b7825d5057677dd1d057e9a3a0bd6fc92133a6a
Luiz Fernando <130947126+OLuizFernando@users.noreply.github.com>
2024-11-08 18:40:22 +0000

    test: rename all descriptions to use new pattern

--- tests/integration/api/v1/status/get.test.js
+++ tests/integration/api/v1/status/get.test.js
@@ -4,25 +4,31 @@ beforeAll(async () => {
   await orchestrator.waitForAllServices();
 });
 
-test("GET to /api/v1/status should return 200", async () => {
-  const response = await fetch("http://localhost:3000/api/v1/status");
-  expect(response.status).toBe(200);
+describe("GET /api/v1/status", () => {
+  describe("Anonymous user", () => {
+    test("Retrieving current system status", async () => {
+      const response = await fetch("http://localhost:3000/api/v1/status");
+      expect(response.status).toBe(200);
 
-  const responseBody = await response.json();
+      const responseBody = await response.json();
 
-  expect(Object.keys(responseBody).length).toBe(2);
-  expect(Object.keys(responseBody.dependencies).length).toBe(1);
-  expect(Object.keys(responseBody.dependencies.database).length).toBe(3);
+      expect(Object.keys(responseBody).length).toBe(2);
+      expect(Object.keys(responseBody.dependencies).length).toBe(1);
+      expect(Object.keys(responseBody.dependencies.database).length).toBe(3);
 
-  expect(responseBody.updated_at).toBeDefined();
-  expect(responseBody.dependencies.database.max_connections).toBeDefined();
-  expect(responseBody.dependencies.database.opened_connections).toBeDefined();
-  expect(responseBody.dependencies.database.version).toBeDefined();
+      expect(responseBody.updated_at).toBeDefined();
+      expect(responseBody.dependencies.database.max_connections).toBeDefined();
+      expect(
+        responseBody.dependencies.database.opened_connections,
+      ).toBeDefined();
+      expect(responseBody.dependencies.database.version).toBeDefined();
 
-  const parsedUpdatedAt = new Date(responseBody.updated_at).toISOString();
-  expect(responseBody.updated_at).toEqual(parsedUpdatedAt);
+      const parsedUpdatedAt = new Date(responseBody.updated_at).toISOString();
+      expect(responseBody.updated_at).toEqual(parsedUpdatedAt);
 
-  expect(responseBody.dependencies.database.max_connections).toBe(100);
-  expect(responseBody.dependencies.database.opened_connections).toBe(1);
-  expect(responseBody.dependencies.database.version).toBe("16.0");
+      expect(responseBody.dependencies.database.max_connections).toBe(100);
+      expect(responseBody.dependencies.database.opened_connections).toBe(1);
+      expect(responseBody.dependencies.database.version).toBe("16.0");
+    });
+  });
 });
